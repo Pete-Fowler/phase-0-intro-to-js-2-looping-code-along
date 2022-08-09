4d2b9dfac719004992c54020cfdde386fa2a1b77
Pete Fowler <fowler.pete@gmail.com>
2022-08-09 10:21:27 -0600

    commit tree 5ef96b83c619f075724ee8c97955f85f4ee2c658

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const writeCards = (arr, msg) => {
+  const newArr = [];
+  for(const person in arr) {
+    newArr.push(`Thank you, ${arr[person]}, for the wonderful ${msg} gift!`);
+  }
+  return newArr;
+}
+
+const countDown = (n) => {
+  for(let i = n; i >=0; i--) {
+    console.log(i);
+  }
+}
